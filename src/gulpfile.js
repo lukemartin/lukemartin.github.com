79add99a8834f9ab4576ddf72eb0d37149798f98
Luke Martin <luke@lukemartin.me>
2016-01-04 17:47:32 +0000

    JS build system

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,11 +1,21 @@
 // Imports
 var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
 var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();
-var shell = require('gulp-shell');
+
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+
+var eslint = require('gulp-eslint');
 
 var sass = require('gulp-sass');
 var bourbon = require('node-bourbon');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
 
 var header = require('gulp-header');
 var pkg = require('./package.json');
@@ -22,14 +32,40 @@ var banner = ['/**',
   ''].join('\n');
 
 
+// Functions
+function compileJS(watch) {
+  var bundler = browserify('js/app.js', { debug: true }).transform(babelify);
+
+  function bundle() {
+    console.log('~> Compiling JS');
+
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('app.js'))
+      .pipe(buffer())
+      .pipe(header(banner, { pkg: pkg }))
+      .pipe(gulp.dest('contents/js'));
+  }
+
+  if (watch) {
+    bundler = watchify(bundler);
+
+    bundler.on('update', bundle);
+  }
+
+  bundle();
+}
+
+
 // Tasks
 gulp.task('serve', ['sass'], function() {
   browserSync.init({
     proxy: 'http://localhost:8991'
   });
 
+  compileJS(true);
   gulp.watch(['scss/**/*.scss'], ['sass']);
-  gulp.watch(['**/*.html'], browserSync.reload);
+  gulp.watch(['**/*.html', 'contents/js/**/*.js'], browserSync.reload);
 });
 
 gulp.task('sass', function() {
@@ -50,13 +86,15 @@ gulp.task('sass', function() {
 gulp.task('build', function() {
   console.log('~> Compiling Sass');
 
-  return gulp.src('scss/app.scss')
+  gulp.src('scss/app.scss')
     .pipe(sass({
       errLogToConsole: true,
       includePaths: bourbon.includePaths
     }))
     .pipe(header(banner, { pkg: pkg }))
-    .pipe(gulp.dest('./contents/css'))
+    .pipe(gulp.dest('./contents/css'));
+
+  compileJS();
 });
 
 // Commands
---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class Cake {
+  constructor() {
+    console.log('hi');
+  }
+}
+
+var cake = new Cake();
+
